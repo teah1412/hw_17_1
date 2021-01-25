cb6c741a80fb48f408d97a69cd0ac01ad2154e7b
John Doe <matea.bertovic@gmail.com>
2021-01-25 17:54:31 +0100

    Prva verzija aplikacije

---
+++ main.py
@@ -0,0 +1,42 @@
+# zadaća 17.1: Secret number game s cookies
+
+import random
+from flask import Flask, render_template, request, make_response
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    secret_number = request.cookies.get("secret_number")
+
+    response = make_response(render_template("index.html"))
+    if not secret_number:
+        new_secret = random.randint(1, 30)
+        response.set_cookie("secret_number", str(new_secret))
+
+    return response
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    guess = int(request.form.get("guess"))
+    secret_number = int(request.cookies.get("secret_number"))
+
+    if guess == secret_number:
+        message = "Correct! The secret number is {0}".format(str(secret_number))
+        response = make_response(render_template("result.html", message=message))
+        response.set_cookie("secret_number", str(random.randint(1, 30)))
+        return response
+
+    elif guess > secret_number:
+        message = "Your guess is not correct... try something smaller."
+        return render_template("result.html", message=message)
+
+    elif guess < secret_number:
+        message = "Your guess is not correct... try something bigger."
+        return render_template("result.html", message=message)
+
+
+
+if __name__ == '__main__':
+    app.run()
